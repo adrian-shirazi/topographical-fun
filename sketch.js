8c26afa7f557c73a510cfacfe348eab87346f084
ashirazi01 <adrian01@vt.edu>
2025-05-28 13:55:18 -0400

    Break out terrain generation into separate method

--- sketch.js
+++ sketch.js
@@ -8,19 +8,39 @@ function setup() {
   const lineColor = "#000000";
   // Would be kind of dope to have a gradient (ie darker = higher, lighter = base)
   let terrain = [];
-  const smoothness = 0.2; // Controls how extreme the terrain is
+  const frequency = 0.2; // Controls how extreme the terrain is
 
-  const cols = width / scale;
-  const rows = height / scale;
+  create2dScalarField(width, height, frequency);
 
-  // Create 2d array
-  for (let x = 0; x < cols; x++) {
-    terrain[x] = [];
-    for (let y = 0; y < rows; y++) {
-      let noiseVal = noise(x * smoothness, y * smoothness);
-      terrain[x][y] = noiseVal;
-      rect(x * scale * 2, y * scale * 2, scale * 2, scale * 2);
-      text(noiseVal.toFixed(2), x * scale * 2, y * scale * 2);
+  /**
+   * Randomly creates a 2d array of floating point values between 0 and 1
+   * @param width - width of canvas
+   * @param height - height of canvas
+   * @param frequency - How extreme the terrain is
+   */
+  function create2dScalarField(width, height, frequency) {
+    const cols = width / scale;
+    const rows = height / scale;
+
+    for (let x = 0; x < cols; x++) {
+      terrain[x] = [];
+      for (let y = 0; y < rows; y++) {
+        let noiseLevel = noise(x * frequency, y * frequency);
+        terrain[x][y] = noiseLevel;
+        drawCell(x, y, scale, noiseLevel);
+      }
     }
+  }
+
+  /**
+   * Draws a cell with the noiseLevel in it (used for visualizing)
+   * @param x - x coord
+   * @param y - y coord
+   * @param scale - Controls resultion
+   * @param noiseLevel - Perlin noise value
+   */
+  function drawCell(x, y, scale, noiseLevel) {
+    rect(x * scale * 2, y * scale * 2, scale * 2, scale * 2);
+    text(noiseLevel.toFixed(2), x * scale * 2, y * scale * 2);
   }
 }
